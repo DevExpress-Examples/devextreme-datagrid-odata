a2a2f64b2b5410896076a3a6dc80fba9726d90ed
Mark Allen Ramirez <markallenramirez@gmail.com>
2025-11-20 20:04:36 +0800

    add vue

--- Vue/src/components/HomeContent.vue
+++ Vue/src/components/HomeContent.vue
@@ -1,28 +1,58 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-
 import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import DxButton from 'devextreme-vue/button';
+import DxDataGrid from 'devextreme-vue/data-grid';
+import { DataSource, ODataStore } from 'devextreme-vue/common/data';
 
-const props = defineProps({
-  text: {
-    type: String,
-    default: 'count',
-  },
+const dataSource = new DataSource({
+  store: new ODataStore({
+    version: 4,
+    url: 'http://localhost:5005/odata/Products',
+    key: 'Product_ID',
+  }),
+  select: [
+    'Product_ID',
+    'Product_Name',
+    'Product_Cost',
+    'Product_Sale_Price',
+    'Product_Retail_Price',
+    'Product_Current_Inventory',
+  ],
+  filter: ['Product_Current_Inventory', '>', 0],
 });
-const count = ref(0);
-const buttonText = computed<string>(
-  () => `Click ${props.text}: ${count.value}`
-);
-function clickHandler() {
-  count.value += 1;
-}
 </script>
 <template>
   <div>
-    <DxButton
-      :text="buttonText"
-      @click="clickHandler"
-    />
+    <DxDataGrid
+      :data-source="dataSource"
+      :show-borders="true"
+    >
+      <DxColumn data-field="Product_ID"/>
+      <DxColumn
+        :width="250"
+        data-field="Product_Name"
+      />
+      <DxColumn
+        data-field="Product_Cost"
+        caption="Cost"
+        data-type="number"
+        format="currency"
+      />
+      <DxColumn
+        data-field="Product_Sale_Price"
+        caption="Sale Price"
+        data-type="number"
+        format="currency"
+      />
+      <DxColumn
+        data-field="Product_Retail_Price"
+        caption="Retail Price"
+        data-type="number"
+        format="currency"
+      />
+      <DxColumn
+        data-field="Product_Current_Inventory"
+        caption="Inventory"
+      />
+    </DxDataGrid>
   </div>
 </template>
