dc825b2b1c1ae36e2f6f52791ab890ef135f130d
Mark Allen Ramirez <markallenramirez@gmail.com>
2025-11-20 18:58:20 +0800

    add odataserver

---
+++ ODataServer/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using ODataServer.Models;
+
+namespace ODataServer.Controllers
+{
+    public class ProductsController : ODataController
+    {
+        [EnableQuery]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return Ok(SampleData.Products);
+        }
+    }
+}
---
+++ ODataServer/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ODataServer.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Product_ID { get; set; }
+        public string? Product_Name { get; set; }
+        public bool Product_Available { get; set; }
+        public int Product_Current_Inventory { get; set; }
+        public string? Product_Cost { get; set; }
+        public string? Product_Sale_Price { get; set; }
+        public string? Product_Retail_Price { get; set; }
+        public double Product_Consumer_Rating { get; set; }
+        public string? Product_Category { get; set; }
+    }
+}
