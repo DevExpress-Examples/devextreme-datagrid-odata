b25079b0ae0e5c6b523830365bea4eaff03f16c5
Mark Allen Ramirez <markallenramirez@gmail.com>
2025-11-21 15:29:17 +0800

    convert fields

--- ODataServer/Models/Product.cs
+++ ODataServer/Models/Product.cs
@@ -10,9 +10,9 @@ namespace ODataServer.Models
         public DateTime Product_Production_Start { get; set; }
         public bool Product_Available { get; set; }
         public int Product_Current_Inventory { get; set; }
-        public string? Product_Cost { get; set; }
-        public string? Product_Sale_Price { get; set; }
-        public string? Product_Retail_Price { get; set; }
+        public double Product_Cost { get; set; }
+        public double Product_Sale_Price { get; set; }
+        public double Product_Retail_Price { get; set; }
         public double Product_Consumer_Rating { get; set; }
         public string? Product_Category { get; set; }
     }
