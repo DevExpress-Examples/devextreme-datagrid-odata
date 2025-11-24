71f8366f431b4d0f716565a0aec10f695857e95a
Mark Allen Ramirez <markallenramirez@gmail.com>
2025-11-24 17:45:23 +0800

    Add React example (#4)

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,16 +1,66 @@
-import { useCallback, useState } from 'react';
 import './App.css';
 import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import Button from 'devextreme-react/button';
+import DataGrid, { Column } from 'devextreme-react/data-grid';
+import { DataSource, ODataStore } from 'devextreme/common/data';
+
+const dataSource = new DataSource({
+  store: new ODataStore({
+    version: 4,
+    url: 'http://localhost:5005/odata/Products',
+    key: 'Product_ID',
+  }),
+  select: [
+    'Product_ID',
+    'Product_Name',
+    'Product_Production_Start',
+    'Product_Cost',
+    'Product_Sale_Price',
+    'Product_Retail_Price',
+    'Product_Current_Inventory',
+  ],
+  filter: ['Product_Current_Inventory', '>', 0],
+});
 
 function App(): JSX.Element {
-  var [count, setCount] = useState<number>(0);
-  const clickHandler = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, [setCount]);
   return (
     <div className="main">
-      <Button text={`Click count: ${count}`} onClick={clickHandler} />
+      <DataGrid
+        dataSource={dataSource}
+        showBorders={true}
+      >
+        <Column dataField="Product_ID" />
+        <Column
+          dataField="Product_Name"
+          width={250}
+        />
+        <Column
+          dataField="Product_Production_Start"
+          caption="Production Start"
+          dataType="datetime"
+        />
+        <Column
+          dataField="Product_Cost"
+          caption="Cost"
+          dataType="number"
+          format="currency"
+        />
+        <Column
+          dataField="Product_Sale_Price"
+          caption="Sale Price"
+          dataType="number"
+          format="currency"
+        />
+        <Column
+          dataField="Product_Retail_Price"
+          caption="Retail Price"
+          dataType="number"
+          format="currency"
+        />
+        <Column
+          dataField="Product_Current_Inventory"
+          caption="Inventory"
+        />
+      </DataGrid>
     </div>
   );
 }
