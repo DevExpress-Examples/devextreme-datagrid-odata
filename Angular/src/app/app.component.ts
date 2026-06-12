f96d09cd62c3e0e08b91cf7ef00c9bcffe2fef30
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-12 12:29:10 +0300

    Update Angular to v22

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { DataSource, ODataStore } from 'devextreme-angular/common/data';
 import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
 
@@ -6,6 +6,7 @@ import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
   imports: [DxDataGridModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
